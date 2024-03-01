c97267a85b19bd2fb047b2fd8790fa6a306461c4
revforev <dutulescu_andrei@yahoo.com>
2024-03-01 13:32:44 +0000

    added mapbox component

--- src/Map.js
+++ src/Map.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import mapbox from 'mapbox-gl';
+import './Map.css';
 
-function Map() {
-  mapboxgl.accessToken = '';
-  let map = new mapbox.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-  });
+class Map extends Component {
+  componentDidMount() {
+    mapbox.accessToken =
+      'pk.eyJ1IjoicmV2Zm9yZXYiLCJhIjoiY2x0OG0zamxjMG8ybTJqcjRyanlwdHo0NSJ9.Me4S5k_QR_oDNLS8ISOUcg';
+    let map = new mapbox.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+    });
+  }
 
-  return <div id="map">map will go here!!</div>;
+  render() {
+    return <div id="map">map will go here!!</div>;
+  }
 }
 
 export default Map;
